303b4958a1a1b6681a0baa5be64d493766d130ab
Alexis <alexisr1993@gmail.com>
2018-07-06 17:29:16 -0700

    Implemented Remove Button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: ['ABC', 'DEF'],
+      list: ['ABC', 'DEF', 'GHI'],
       userInput: ''
     }
     this.handleClick = this.handleClick.bind(this);
@@ -14,8 +14,11 @@ class App extends Component {
     this.handleComplete = this.handleComplete.bind(this);
 
   }
-  handleRemove() {
-
+  handleRemove(value) {
+    this.state.list.splice(this.state.list.indexOf(value), 1)
+    this.setState ({
+      list: this.state.list
+    });
   }
   handleComplete(){
 
@@ -25,14 +28,14 @@ class App extends Component {
     console.log("Value in handleClick " + this.state.userInput)
     this.setState ({
       list: this.state.list.concat(this.state.userInput)
-    })
+    });
     console.log("I Am handling the CLICK");
     console.log(this.state.list);
   }
   handleChange(value){
     this.setState ({
       userInput: value
-    })
+    });
     console.log("I Am handling the CHANGE");
     console.log(value);
   }
@@ -75,7 +78,7 @@ class List extends React.Component {
     return (
       <div className="list-div">
         <ul>
-          {this.props.listState.map(x => <ListItem  element={x}></ListItem>)}
+          {this.props.listState.map(x => <ListItem handleChange={this.props.handleChange} handleRemove={this.props.handleRemove} element={x}></ListItem>)}
         </ul>
       </div>
     );
@@ -83,16 +86,25 @@ class List extends React.Component {
   }
 }
 class RemoveButton extends React.Component {
+  constructor(props){
+    super(props);
+    this.onRemove = this.onRemove.bind(this);
+  }
+  onRemove(event){
+    const value = event.target.value;
+    console.log(value);
+    this.props.handleRemove(value);
+  }
   render() {
     return (
-      <button className="Remove-button">X</button>
+      <button className="Remove-button" value={this.props.value} onClick={this.onRemove}>X</button>
     );
   }
 }
 class CompleteButton extends React.Component {
   render() {
     return (
-      <button className="Compelte-button">&#10003;</button>
+      <button className="Compelte-button" onClick={this.props.handleComplete}>&#10003;</button>
     );
   }
 }
@@ -100,7 +112,7 @@ class CompleteButton extends React.Component {
 class ListItem extends React.Component {
   render() {
     return (
-      <li className="List-element" key={this.props.element}> <CompleteButton></CompleteButton><RemoveButton></RemoveButton> {this.props.element}</li>
+      <li className="List-element" > <CompleteButton></CompleteButton><RemoveButton handleRemove={this.props.handleRemove} value={this.props.element}></RemoveButton> {this.props.element}</li>
     );
   }
 }
